dd2e8cf38ff3f03d7c3589a74805eab5ce84bd5b
Khalid <timoncr9@gmail.com>
2018-08-26 09:40:39 +0200

    Comments added

--- main.js
+++ main.js
@@ -7,16 +7,21 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
-        this.nonce = 0;
+        this.nonce = 0; //This variable is to prevent mining forever in the while loop at line 18 by changing some non-used value in the hash 
+        //function so that the hash is different each time.
     }
 
+    //This function generates a random hash using the SHA256 built-in function to generate a random hash using 
+    //the variables associated with each block
     calculateHash() {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
+    //This function is what make sure that the hash corresponding to a block fulfills the difficulty set by the
+    //admin
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
+            this.nonce++; //incrementing to change some value in the hash so that the loop doesn't go forever
             this.hash = this.calculateHash();
         }
 
@@ -27,7 +32,7 @@ class Block {
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 4; //Manually set
     }
 
     createGenesisBlock() {
